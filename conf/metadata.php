2575c6c3a53421ebeeaac1d5de6d6e2cba0774ac
Leo Zachl <leo.zachl@gmail.com>
2016-01-14 12:47:48 +0100

    make scope (fb-permissions) configurable

--- conf/metadata.php
+++ conf/metadata.php
@@ -6,6 +6,8 @@
  */
 $meta['applicationID'] = array('string');
 $meta['applicationSecret'] = array('string');
+$meta['appAdmin'] = array('string');
 $meta['superuser'] = array('string');
 $meta['defaultgroup'] = array('string');
 $meta['fbgid2group'] = array('string');
+$meta['scope'] = array('string');
